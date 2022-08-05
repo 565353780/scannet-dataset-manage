305836ee99b64beb654985fbbb2a22e44e83fae7
chLi <565353780@qq.com>
2022-08-05 18:52:59 +0800

    finish load most file paths

--- Data/scene.py
+++ Data/scene.py
@@ -12,6 +12,7 @@ class Scene(object):
         self.scene_name = None
         self.space_id = None
         self.scan_id = None
+        self.file_basepath = None
 
         self.sens = None
         self.vh_clean_ply = None
@@ -19,20 +20,80 @@ class Scene(object):
         self.aggregation_json = None
         self.vh_clean_aggregation_json = None
         self.txt = None
+
+        self.update()
         return
+
+    def updateSceneId(self):
+        self.scene_name = self.scene_folder_path.split("/")[-2]
+        if "scene" not in self.scene_name:
+            print("[ERROR][Scene::updateSceneId]")
+            print("\t scene_folder_name not valid!")
+            return False
+
+        self.space_id, self.scan_id = self.scene_name.split("scene")[1].split("_")
+        return True
+
+    def updateFilePath(self):
+        self.file_basepath = self.scene_folder_path + self.scene_name
+
+        not_exist_file_path_list = []
+
+        sens = self.file_basepath + ".sens"
+        if os.path.exists(sens):
+            self.sens = sens
+        else:
+            not_exist_file_path_list.append(sens)
+
+        vh_clean_ply = self.file_basepath + "_vh_clean.ply"
+        if os.path.exists(vh_clean_ply):
+            self.vh_clean_ply = vh_clean_ply
+        else:
+            not_exist_file_path_list.append(vh_clean_ply)
+
+        vh_clean_segs_json = self.file_basepath + "_vh_clean.segs.json"
+        if os.path.exists(vh_clean_segs_json):
+            self.vh_clean_segs_json = vh_clean_segs_json
+        else:
+            not_exist_file_path_list.append(vh_clean_segs_json)
+
+        aggregation_json = self.file_basepath + ".aggregation.json"
+        if os.path.exists(aggregation_json):
+            self.aggregation_json = aggregation_json
+        else:
+            not_exist_file_path_list.append(aggregation_json)
+
+        vh_clean_aggregation_json = self.file_basepath + "_vh_clean.aggregation.json"
+        if os.path.exists(vh_clean_aggregation_json):
+            self.vh_clean_aggregation_json = vh_clean_aggregation_json
+        else:
+            not_exist_file_path_list.append(vh_clean_aggregation_json)
+
+        txt = self.file_basepath + ".txt"
+        if os.path.exists(txt):
+            self.txt = txt
+        else:
+            not_exist_file_path_list.append(txt)
+
+        if len(not_exist_file_path_list) > 0:
+            print("[WARN][Scene::updateFilePath]")
+            print("\t find some files not exist!")
+            for not_exist_file_path in not_exist_file_path_list:
+                print("\t\t " + not_exist_file_path)
+        return True
 
     def update(self):
         if not os.path.exists(self.scene_folder_path):
             print("[ERROR][Scene::update]")
             print("\t scene_folder not exist!")
             return False
-
-        self.scene_name = self.scene_folder_path.split("/")[-2]
-        if "scene" not in self.scene_name:
+        if not self.updateSceneId():
             print("[ERROR][Scene::update]")
-            print("\t scene_folder_name not valid!")
+            print("\t updateSceneId failed!")
             return False
-
-        self.space_id, self.scan_id = self.scene_name.split("scene")[1].split("_")
+        if not self.updateFilePath():
+            print("[ERROR][Scene::update]")
+            print("\t updateFilePath failed!")
+            return False
         return True
 
