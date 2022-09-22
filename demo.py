ffd155d59523c2c81e49677f186e983373b96ab2
chLi <565353780@qq.com>
2022-09-23 01:19:03 +0800

    add new demo functions

--- demo.py
+++ demo.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from scannet_dataset_manage.Module.object_spliter import demo
+from scannet_dataset_manage.Module.dataset_loader import demo as demo_dataset
+from scannet_dataset_manage.Module.object_spliter import demo as demo_split_object
+from scannet_dataset_manage.Module.object_bbox_generator import demo as demo_generate_object_bbox
+
+
+def autoExtractDataset():
+    demo_split_object()
+    demo_generate_object_bbox()
+    return True
+
 
 if __name__ == "__main__":
-    demo()
+    #  demo_dataset()
+    #  demo_split_object()
+    demo_generate_object_bbox()
 
+    #  autoExtractDataset()
