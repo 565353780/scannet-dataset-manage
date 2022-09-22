f122ac37ee66b913574bec684774731f2d162a7e
chLi <565353780@qq.com>
2022-09-23 01:18:40 +0800

    add 2 more new functions

--- scannet_dataset_manage/Method/path.py
+++ scannet_dataset_manage/Method/path.py
@@ -3,9 +3,21 @@
 
 import os
 
+
+def getFileFolderPath(file_path):
+    file_name = file_path.split("/")[-1]
+    file_folder_path = file_path.split(file_name)[0]
+    return file_folder_path
+
+
+def createFileFolder(file_path):
+    file_folder_path = getFileFolderPath(file_path)
+    os.makedirs(file_folder_path, exist_ok=True)
+    return True
+
+
 def removeIfExist(file_path):
     if not os.path.exists(file_path):
         return True
     os.remove(file_path)
     return True
-
