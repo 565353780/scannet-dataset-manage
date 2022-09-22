497ad19ac85413183f988a6327bd58f70c7d0ec6
chLi <565353780@qq.com>
2022-09-23 01:18:30 +0800

    simplify code

--- scannet_dataset_manage/Module/object_spliter.py
+++ scannet_dataset_manage/Module/object_spliter.py
@@ -2,17 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
 sys.path.append("../mesh-manage/")
+from mesh_manage.Module.channel_mesh import ChannelMesh
 
 import os
 from tqdm import tqdm
 
-from mesh_manage.Module.channel_mesh import ChannelMesh
-
 from scannet_dataset_manage.Method.path import removeIfExist
+
 from scannet_dataset_manage.Module.dataset_loader import DatasetLoader
 
+
 class ObjectSpliter(object):
+
     def __init__(self, dataset_folder_path, save_object_folder_path):
         self.dataset_loader = DatasetLoader(dataset_folder_path)
         self.save_object_folder_path = save_object_folder_path
@@ -25,10 +28,8 @@ class ObjectSpliter(object):
         save_object_basepath = self.save_object_folder_path + scene_name + "/"
 
         scene_mesh_file_path = scene.vh_clean_2_ply
-        if scene_mesh_file_path is None:
-            print("[ERROR][ObjectSpliter::splitScene]")
-            print("\t scene_mesh_file not found!")
-            return False
+        assert scene_mesh_file_path is not None
+
         channel_mesh = ChannelMesh(scene_mesh_file_path)
 
         object_num = scene.getLabeledObjectNum()
@@ -36,10 +37,7 @@ class ObjectSpliter(object):
         print("\t start split object in scene", scene_name, "...")
         for object_idx in tqdm(range(object_num)):
             labeled_object = scene.getLabeledObjectById(object_idx)
-            if labeled_object is None:
-                print("[ERROR][ObjectSpliter::splitObject]")
-                print("\t getLabeledObjectById failed!")
-                return False
+            assert labeled_object is not None
 
             save_object_mesh_file_path = save_object_basepath + \
                 str(labeled_object.object_id) + \
@@ -50,34 +48,33 @@ class ObjectSpliter(object):
             tmp_save_object_mesh_file_path = save_object_mesh_file_path[:-4] + "_tmp.ply"
             removeIfExist(tmp_save_object_mesh_file_path)
 
-            point_idx_list = scene.getPointIdxListByLabeledObject(labeled_object)
-            if not channel_mesh.generateMeshByPoint(point_idx_list, tmp_save_object_mesh_file_path):
-                print("[ERROR][ObjectSpliter::splitObject]")
-                print("\t generateMeshByPoint failed!")
-                return False
+            point_idx_list = scene.getPointIdxListByLabeledObject(
+                labeled_object)
+            assert channel_mesh.generateMeshByPoint(
+                point_idx_list, tmp_save_object_mesh_file_path)
 
-            os.rename(tmp_save_object_mesh_file_path, save_object_mesh_file_path)
+            os.rename(tmp_save_object_mesh_file_path,
+                      save_object_mesh_file_path)
         return True
 
     def splitAll(self):
-        scene_num = self.dataset_loader.getSceneNum()
-        for scene_idx in range(scene_num):
-            scene = self.dataset_loader.getScene(scene_idx)
-            if scene is None:
-                print("[ERROR][ObjectSpliter::splitAll]")
-                print("\t getScene failed!")
-                return False
+        scene_name_list = self.dataset_loader.getSceneNameList()
+        for i, scene_name in enumerate(scene_name_list):
+            scene = self.dataset_loader.getScene(scene_name)
+            assert scene is not None
 
             print("[INFO][ObjectSpliter::splitAll]")
-            print("\t start split scene", scene.scene_name, ",", scene_idx + 1, "/", scene_num, "...")
+            print("\t start split scene", scene.scene_name, ",", i + 1, "/",
+                  len(scene_name_list), "...")
             self.splitScene(scene)
         return True
+
 
 def demo():
     dataset_folder_path = "/home/chli/chLi/ScanNet/scans/"
     save_object_folder_path = "/home/chli/chLi/ScanNet/objects/"
 
-    object_spliter = ObjectSpliter(dataset_folder_path, save_object_folder_path)
+    object_spliter = ObjectSpliter(dataset_folder_path,
+                                   save_object_folder_path)
     object_spliter.splitAll()
     return True
-
