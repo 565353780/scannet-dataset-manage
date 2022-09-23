98f8c5315f31e00d376bc86a3fc08e559c6995a6
chLi <565353780@qq.com>
2022-09-23 14:45:56 +0800

    simplify spliter

--- scannet_dataset_manage/Module/object_spliter.py
+++ scannet_dataset_manage/Module/object_spliter.py
@@ -9,7 +9,7 @@ from mesh_manage.Module.channel_mesh import ChannelMesh
 import os
 from tqdm import tqdm
 
-from scannet_dataset_manage.Method.path import removeIfExist
+from scannet_dataset_manage.Method.path import renameFile, removeIfExist
 
 from scannet_dataset_manage.Module.dataset_loader import DatasetLoader
 
@@ -19,8 +19,6 @@ class ObjectSpliter(object):
     def __init__(self, dataset_folder_path, save_object_folder_path):
         self.dataset_loader = DatasetLoader(dataset_folder_path)
         self.save_object_folder_path = save_object_folder_path
-        if self.save_object_folder_path[-1] != "/":
-            self.save_object_folder_path += "/"
         return
 
     def splitScene(self, scene):
@@ -53,13 +51,18 @@ class ObjectSpliter(object):
             assert channel_mesh.generateMeshByPoint(
                 point_idx_list, tmp_save_object_mesh_file_path)
 
-            os.rename(tmp_save_object_mesh_file_path,
-                      save_object_mesh_file_path)
+            renameFile(tmp_save_object_mesh_file_path,
+                       save_object_mesh_file_path)
         return True
 
     def splitAll(self):
+        # for me only
+        finished_scene_idx = 317
         scene_name_list = self.dataset_loader.getSceneNameList()
         for i, scene_name in enumerate(scene_name_list):
+            if i + 1 < finished_scene_idx:
+                continue
+
             scene = self.dataset_loader.getScene(scene_name)
             assert scene is not None
 
