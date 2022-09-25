39d7b044faf54a7be8ae0f2c7713454e07ba97d0
chLi <565353780@qq.com>
2022-09-25 19:24:34 +0800

    fix import error

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from scannet_dataset_manage.Module.dataset_loader import demo as demo_dataset
-from scannet_dataset_manage.Module.object_spliter import demo as demo_split_object
-from scannet_dataset_manage.Module.object_bbox_generator import demo as demo_generate_object_bbox
+from scannet_dataset_manage.Demo.dataset_loader import demo as demo_dataset
+from scannet_dataset_manage.Demo.object_spliter import demo as demo_split_object
+from scannet_dataset_manage.Demo.object_bbox_generator import demo as demo_generate_object_bbox
 
 
 def autoExtractDataset():
