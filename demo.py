e08eb8b3e220231e62bbc80c422c9a94dadf84da
chLi <565353780@qq.com>
2022-12-13 13:58:06 +0800

    add generate glb func

--- demo.py
+++ demo.py
@@ -4,8 +4,10 @@
 from scannet_dataset_manage.Demo.dataset_loader import demo as demo_load_dataset
 from scannet_dataset_manage.Demo.object_spliter import demo as demo_split_object
 from scannet_dataset_manage.Demo.object_bbox_generator import demo as demo_generate_object_bbox
+from scannet_dataset_manage.Demo.glb_generator import demo as demo_generate_glb
 
 if __name__ == "__main__":
     #  demo_load_dataset()
     demo_split_object()
     demo_generate_object_bbox()
+    demo_generate_glb()
