0bb192109086a11099f7833e5b08361175db9492
chLi <565353780@qq.com>
2023-04-30 21:56:32 +0800

    add read sens demo, finish load header

--- demo.py
+++ demo.py
@@ -5,9 +5,14 @@ from scannet_dataset_manage.Demo.dataset_loader import demo as demo_load_dataset
 from scannet_dataset_manage.Demo.object_spliter import demo as demo_split_object
 from scannet_dataset_manage.Demo.object_bbox_generator import demo as demo_generate_object_bbox
 from scannet_dataset_manage.Demo.glb_generator import demo as demo_generate_glb
+from scannet_dataset_manage.Demo.sens_reader import demo as demo_read_sens
 
 if __name__ == "__main__":
     #  demo_load_dataset()
-    demo_split_object()
-    demo_generate_object_bbox()
-    demo_generate_glb()
+
+    # auto generate dataset data progress
+    #  demo_split_object()
+    #  demo_generate_object_bbox()
+    #  demo_generate_glb()
+
+    demo_read_sens()
---
+++ scannet_dataset_manage/Demo/sens_reader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from scannet_dataset_manage.Module.sens_reader import SensReader
+
+
+def demo():
+    sens_file_path = '/home/chli/chLi/ScanNet/scans/scene0000_01/scene0000_01.sens'
+    sens_file_path = '/home/chli/scene0000_01.sens'
+    header_only = True
+    output_path = './tmp/'
+
+    sens_reader = SensReader(sens_file_path, header_only)
+
+    return
+    sens_reader.export_depth_images(output_path + 'depth/')
+    sens_reader.export_color_images(output_path + 'color/')
+    sens_reader.export_poses(output_path + 'pose/')
+    sens_reader.export_intrinsics(output_path + 'intrinsic/')
+    return True
